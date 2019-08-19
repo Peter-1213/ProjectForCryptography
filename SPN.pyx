af25a785d91914fe3937dbbbeefb3740ba32dc91
uiuu123 <peterpee@vip.qq.com>
2019-08-19 10:51:19 +0800

    Revert "Delete SPN.pyx"
    This reverts commit a3321acc0040ba8c783df815551d768db489f6b0.
    
    # Conflicts:
    #	SPN.pyx

--- SPN.pyx
+++ SPN.pyx
@@ -1,4 +1,4 @@
 #SPN：置换-代换网络。这个模块手动实现了一个SPN
 #根据书上的讲解，SPN
 cdef SPN_base(long long origin, int key, short substitution_box[256], char permutation_box[32], int rnd):
-    cpdef output_
+    cpdef output_data
